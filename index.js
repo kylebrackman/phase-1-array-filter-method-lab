c9b10c111707b855311f271f5e8b258e48c5bf77
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-10-03 15:19:21 -0600

    commit tree 5e2f4a36be259472119514601dbce40b4f0d246a

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
+function findMatching(driverNamesArray, string) {
+    return driverNamesArray.filter(name => string.toLowerCase() === name.toLowerCase())
+    }
+
+
+    
+function fuzzyMatch(driverNamesArray, string) {
+    return driverNamesArray.filter(name => string.slice(0,2) === name.slice(0,2))
+}
+
+function matchName(driverObj, string) {
+    return driverObj.filter(obj => obj.name === string)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Code your solution here
+
+// function findMatching(b, a) {
+//     return b.filter(function (c) {return c.toLowerCase() === a.toLowerCase()
+//     })
+// }
+
+// function fuzzyMatch(b, a) {
+//     return b.filter(function (c) { return c.toLowerCase().slice(0, 2) === a.toLowerCase().slice(0, 2)})
+//     }
+
+// function matchName(b, a) {
+//     return b.filter(function (c) {return c.name === a})
+// }
